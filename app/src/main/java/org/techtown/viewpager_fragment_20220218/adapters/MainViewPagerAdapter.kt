93d480a6a23a1dbd07d1eb52d836710694d40909
집윤주 <toyaann@naver.com>
2022-02-18 17:01:26 +0900

    필수 구현 함수 2개 완성  - 총 몇장?  - 각 position에 어떤 프래그먼트가 나가는지

--- app/src/main/java/org/techtown/viewpager_fragment_20220218/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/org/techtown/viewpager_fragment_20220218/adapters/MainViewPagerAdapter.kt
@@ -1,7 +1,26 @@
 package org.techtown.viewpager_fragment_20220218.adapters
 
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import org.techtown.viewpager_fragment_20220218.fragments.BirthYearFragment
+import org.techtown.viewpager_fragment_20220218.fragments.HelloFragment
+import org.techtown.viewpager_fragment_20220218.fragments.NameFragment
 
 class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+        return 3
+    }
+//                        각 포지션에 어떤 프래그먼트 나가?
+    override fun getItem(position: Int): Fragment {
+        if(position == 0){
+            return NameFragment()
+        }
+        else if (position == 1) {
+            return BirthYearFragment()
+        }
+        else {
+            return HelloFragment()
+        }
+    }
 }
