2fcb9180f7aeeb673d516eec057207137ab2388e
집윤주 <toyaann@naver.com>
2022-02-18 16:50:05 +0900

    프래그먼트 어댑터 클래스 추가  - FragmentPagerAdapter 상속  - 기본생성자 지원 X

---
+++ app/src/main/java/org/techtown/viewpager_fragment_20220218/fragments/BirthYearFragment.kt
@@ -0,0 +1,23 @@
+package org.techtown.viewpager_fragment_20220218.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.techtown.viewpager_fragment_20220218.R
+
+class BirthYearFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_name, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
