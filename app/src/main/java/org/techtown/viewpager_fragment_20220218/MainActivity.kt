dc194c6f86bb2d85a825881860f21581a96ecdcd
집윤주 <toyaann@naver.com>
2022-02-18 17:06:50 +0900

    어댑터 객체화 -> 액티비티에 있는 ViewPager 의 어댑터로 연결

--- app/src/main/java/org/techtown/viewpager_fragment_20220218/MainActivity.kt
+++ app/src/main/java/org/techtown/viewpager_fragment_20220218/MainActivity.kt
@@ -2,10 +2,29 @@ package org.techtown.viewpager_fragment_20220218
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.techtown.viewpager_fragment_20220218.adapters.MainViewPagerAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mAdapter : MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupEvents()
+        setValues()
+
+    }
+
+    fun setupEvents(){}
+
+    fun setValues(){
+
+        mAdapter = MainViewPagerAdapter( supportFragmentManager )
+        mainViewPager.adapter = mAdapter
+
+
     }
 }
