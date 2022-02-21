b3b4ef8b3a52aa127c755bd9991271c5cf9b9a83
집윤주 <toyaann@naver.com>
2022-02-21 10:11:25 +0900

    탭 레이아웃 배치 / 뷰페이저 연결

--- app/src/main/java/org/techtown/viewpager_fragment_20220218/MainActivity.kt
+++ app/src/main/java/org/techtown/viewpager_fragment_20220218/MainActivity.kt
@@ -28,6 +28,9 @@ class MainActivity : AppCompatActivity() {
 //        재 사용성 무시하도록 하는 코드 (사용성 개선)
         mainViewPager.offscreenPageLimit = 5
 
+//        탭 레이아웃 / 뷰페이저 연결
+        mainTabLayout.setupWithViewPager(mainViewPager)
+
 
     }
 }
